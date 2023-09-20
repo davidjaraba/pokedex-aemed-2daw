19f950b05cf15c63e89ba8e80b1ce06e98bd4a5d
velcas <velcas669@gmail.com>
2023-09-20 20:48:50 +0200

    Agregado Mockito

--- POKEDEX/build.gradle.kts
+++ POKEDEX/build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("com.google.code.gson:gson:2.10.1")
+    testImplementation("org.mockito:mockito-core:5.5.0")
 }
 
 tasks.test {
